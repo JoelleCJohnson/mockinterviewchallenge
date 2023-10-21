db11e4f8440643dbae14f993d4527443af94056f
Joelle Johnson <119087783+JoelleCJohnson@users.noreply.github.com>
2023-10-21 09:00:24 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-//write a function that takes an array as a parameter and adds the sum of all items in the array. 
+//Challenge- Write a function that takes an array as a parameter and adds the sum of all items in the array. 
 
-function arrayTotal(array){
-    let total = 0
-    for (let i = 0; i < array.length; i++){
-        total += array[i]
+//Defining the function:
+function arrayTotal(array){ 
+    let total = 0                               //Set the counting varibale to 0.
+    for (let i = 0; i < array.length; i++){    //Loops through the array.
+        total += array[i]                     //Adds the value of the current array element to the value of total.
     }
-return total
+return total                     //Returns the value of total after the loop executes.
 }
